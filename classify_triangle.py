7243a9b5f98d1bd214bc0ae5023ae1c54dcf5410
Cesar Espejo <cespejo@stevens.edu>
2024-03-02 23:22:06 -0500

    Updated HW01.py

--- classify_triangle.py
+++ classify_triangle.py
@@ -1,12 +1,13 @@
 """Triangle Classification Function"""
 class BuggyTriangle:
-    """Class for Triangle Classification Function"""
+    """Constructor"""
     def __init__(self, s1, s2, s3):
         self.a = s1
         self.b = s2
         self.c = s3
 
     def right_check(self):
+        """Checks if a Triangle contains a right angle, returns false if otherwise"""
         x = round(self.a ** 2, 4)
         y = round(self.b ** 2, 4)
         z = round(self.c ** 2, 4)
@@ -16,6 +17,7 @@ class BuggyTriangle:
             return False
 
     def classify_triangle(self):
+        """Classifies triangle and type"""
         if self.a + self.b <= self.c or self.a + self.c <= self.b or self.b + self.c <= self.a:
             return "Invalid Triangle"
         elif self.a == self.b:
